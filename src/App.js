823827635160043ab9cf7486757f3771362f5e51
Ajani Opeyemi <ooajani@outlook.com>
2021-08-28 18:21:55 +0100

    - WIP : Design in Progress.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './company_shift_logo.png';
+import company from './company_shift.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+        <div className="cs-left split">
+          <div className="cs-logo">
+            <img src={logo} className="cs-logo_img" alt="Employee Shift Logo"/>
+            <h3 className="cs-name">ZURI</h3>
+          </div>
+          <div>
+              <h2 className="cs-head">Shift Management simplified - Task 18</h2>
+
+              <p className="cs-built">Bulit by team curies</p>
+
+              <button className="cs-button">Get started</button>
+          </div>
+        </div>
+        <div className="cs-right split">
+            <img src={company} className="cs-company_img" alt="Employee Shift"/>
+          </div>
       </header>
+      
     </div>
   );
 }
