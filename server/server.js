8541c52378973656ff943bc47f0fbfa8fd433905
Aviator <inboxlegacy@gmail.com>
2021-09-02 01:57:25 +0100

    chore: setup backend to serve react client

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+//app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api', (req,res)=> {
+  return res.json({
+    message: 'Employee Shift API Success!!!'
+  })
+})
+
+
+
+//render react app index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
+
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
