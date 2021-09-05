0245061554aefa5c8f5280244d5f3cdb9b1f30cc
stephennwac007 <stephennwac007@gmail.com>
2021-09-05 01:26:22 +0100

    added chatbot feature v1.1

--- client/src/chatbot/config.js
+++ client/src/chatbot/config.js
@@ -2,7 +2,7 @@ import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
     botname: "ZuriBot",
-	initialMessages: [createChatBotMessage(`Hello world`)],
+	initialMessages: [createChatBotMessage(`Hello world, welcome to zuri chat app`)],
 };
 
 export default config;
