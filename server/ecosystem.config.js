d126a59118fdf8a8897a7ec2646854601580d0fa
Aviator <inboxlegacy@gmail.com>
2021-09-09 01:38:20 +0100

    add eslint config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    apps: [
-            {
-            name: "server",
-            script: "server.js",
-            env: {
-                NODE_ENV: "development",
-            },
-            env_staging: {
-                NODE_ENV: "staging",
-            },
-            env_production: {
-                NODE_ENV: "production",
-            },
-        },
-    ],
-};
+	apps: [
+		{
+			name: "server",
+			script: "server.js",
+			env: {
+				NODE_ENV: "development",
+			},
+			env_staging: {
+				NODE_ENV: "staging",
+			},
+			env_production: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+}
---
+++ server/src/server.js
@@ -0,0 +1,49 @@
+require("dotenv").config()
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+const PORT = process.env.PORT || 5356
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")))
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("/api", (req, res) => res.json({
+	message: "Employee Shift API Success!!!",
+}))
+
+// categories employees roles api issue #97
+app.get("/api/users", (req, res) => res.json([
+	{
+		id: 1,
+		name: "Ushahemba Shir",
+		title: "Back end dev",
+	},
+	{
+		id: 2,
+		name: "Solomon Ifianyi",
+		title: "Back end dev",
+	},
+	{
+		id: 3,
+		name: "Depecode",
+		title: "Front end dev",
+	},
+	{
+		id: 4,
+		name: "John",
+		title: "Dev ops",
+	},
+]))
+
+// render react app index.html
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
+})
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
