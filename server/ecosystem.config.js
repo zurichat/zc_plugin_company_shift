cd9e45768573e2956e2121e76d7fba8763a58298
John Okwajebi <69980990+johnokwajebi@users.noreply.github.com>
2021-09-13 11:06:05 +0100

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
+
     apps: [
             {
             name: "server",
@@ -15,26 +15,25 @@ module.exports = {
             },
         },
     ],
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> ceaa8d3bd900becec3d7c0a354d11487cfce51dc
-=======
-	apps: [
-		{
-			name: "server",
-			script: "server.js",
-			env: {
-				NODE_ENV: "development",
-			},
-			env_staging: {
-				NODE_ENV: "staging",
-			},
-			env_production: {
-				NODE_ENV: "production",
-			},
-		},
-	],
-}
->>>>>>> 242ba9db57f51ff3a4cba488b4a5af2354eb14db
+// =======
+// };
+// >>>>>>> ceaa8d3bd900becec3d7c0a354d11487cfce51dc
+// =======
+// 	apps: [
+// 		{
+// 			name: "server",
+// 			script: "server.js",
+// 			env: {
+// 				NODE_ENV: "development",
+// 			},
+// 			env_staging: {
+// 				NODE_ENV: "staging",
+// 			},
+// 			env_production: {
+// 				NODE_ENV: "production",
+// 			},
+// 		},
+// 	],
+// }
+// >>>>>>> 242ba9db57f51ff3a4cba488b4a5af2354eb14db
