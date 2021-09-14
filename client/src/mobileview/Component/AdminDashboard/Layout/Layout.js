5b2de583c888572037da860bf5b16042869feb62
stephen <akinfolarinstephen8@gmail.com>
2021-09-14 14:51:57 +0100

    working admin dashboard

---
+++ client/src/mobileview/Component/AdminDashboard/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from '../Main/Main';
+import Header from '../header/Header';
+
+function Layout() {
+  return (
+    <div>
+      <Main></Main>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ client/src/mobileview/Component/AdminDashboard/header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return <div>my header</div>;
+}
+
+export default Header;
